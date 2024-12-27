4e8e0c5748a089f6a4451f363b0a92bf1ed23ddc
aagargoura <ahmed.gargoura@gmail.com>
2024-12-27 18:53:31 +0100

    Add Hedera NFT

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction } = require("@hashgraph/sdk")
+console.clear();
 require('dotenv').config();
 
+const {
+  Client,
+  PrivateKey,
+  AccountCreateTransaction,
+  AccountBalanceQuery,
+  Hbar,
+  TransferTransaction,
+  TokenCreateTransaction,
+  TokenType,
+  TokenSupplyType,
+  TokenAssociateTransaction,
+  TokenMintTransaction
+} = require("@hashgraph/sdk")
 
-async function environmentSetup() {
+const supplyKey = PrivateKey.generateECDSA();
+
+//Create the NFT
+async function nftCreate() {
   // STEP 1: Set up environment variables and creates a testnet client with account as the operator account.
   // Grab you Hedera testnet account ID and private key from your .env file
   const myAccountId = process.env.MY_ACCOUNT_ID;
@@ -63,24 +79,126 @@ async function environmentSetup() {
   const accountBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
   console.log("The new account balance is: " + accountBalance.hbars.toTinybars() + " tinybar.");
 
-  //Create the transfer transaction
-  const sendHbar = await new TransferTransaction().addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000)).addHbarTransfer(newAccountId, Hbar.fromTinybars(1000)).execute(client);
+  /* ---------------- Create a Non-Fungible Token (NFT) ---------------- */
+  //Create the NFT
+  const nftCreate = await new TokenCreateTransaction()
+    .setTokenName("Hedera NFT")
+    .setTokenSymbol("HNFT")
+    .setTokenType(TokenType.NonFungibleUnique)
+    .setDecimals(0)
+    .setInitialSupply(0)
+    .setTreasuryAccountId(myAccountId)
+    .setSupplyType(TokenSupplyType.Finite)
+    .setMaxSupply(250)
+    .setSupplyKey(supplyKey)
+    .freezeWith(client);
 
-  //Verify the transaction reached consensus
-  const transactionReceipt = await sendHbar.getReceipt(client);
-  console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
+  //Log the supply Key
+  console.log("- Supply Key: " + supplyKey);
 
-  //Request the cost of the query
-  const queryCost = await new AccountBalanceQuery().setAccountId(newAccountId).getCost(client);
-  console.log("The cost of query is: " + queryCost);
+  //Sign the transaction with the treasury key
+  const nftCreateTxSign = await nftCreate.sign(PrivateKey.fromString(myPrivateKey));
 
-  //Check the new account's balance
-  const getNewBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
-  console.log("The account balance after the transfer is: " + getNewBalance.hbars.toTinybars() + " tinybar.")
+  //Submit the transaction to a Hedera network
+  const nftCreateSubmit = await nftCreateTxSign.execute(client);
 
-  console.log("End");
-  client.close();
-  return newAccountId;
+  //Get the transaction receipt
+  const nftCreateRx = await nftCreateSubmit.getReceipt(client);
+
+  //Get the token ID
+  const tokenId = nftCreateRx.tokenId;
+
+  //Log the token ID
+  console.log("Created NFT with Token ID: " + tokenId);
+
+  /* ---------------- Mint a New NFT ---------------- */
+  // Max transaction fee as a constant
+  const maxTransactionFee = new Hbar(20);
+
+  //IPFS content identifiers for which we will create a NFT
+  const CID = [
+    Buffer.from(
+      "ipfs://bafyreiao6ajgsfji6qsgbqwdtjdu5gmul7tv2v3pd6kjgcw5o65b2ogst4/metadata.json"
+    ),
+    Buffer.from(
+      "ipfs://bafyreic463uarchq4mlufp7pvfkfut7zeqsqmn3b2x3jjxwcjqx6b5pk7q/metadata.json"
+    ),
+    Buffer.from(
+      "ipfs://bafyreihhja55q6h2rijscl3gra7a3ntiroyglz45z5wlyxdzs6kjh2dinu/metadata.json"
+    ),
+    Buffer.from(
+      "ipfs://bafyreidb23oehkttjbff3gdi4vz7mjijcxjyxadwg32pngod4huozcwphu/metadata.json"
+    ),
+    Buffer.from(
+      "ipfs://bafyreie7ftl6erd5etz5gscfwfiwjmht3b52cevdrf7hjwxx5ddns7zneu/metadata.json"
+    )
+  ];
+
+  // MINT NEW BATCH OF NFTs
+  const mintTx = new TokenMintTransaction()
+    .setTokenId(tokenId)
+    .setMetadata(CID) //Batch minting - UP TO 10 NFTs in single tx
+    .setMaxTransactionFee(maxTransactionFee)
+    .freezeWith(client);
+
+  //Sign the transaction with the supply key
+  const mintTxSign = await mintTx.sign(supplyKey);
+
+  //Submit the transaction to a Hedera network
+  const mintTxSubmit = await mintTxSign.execute(client);
+
+  //Get the transaction receipt
+  const mintRx = await mintTxSubmit.getReceipt(client);
+
+  //Log the serial number
+  console.log("Created NFT " + tokenId + " with serial number: " + mintRx.serials);
+
+  /* ---------------- Associate User Accounts with the NFT ---------------- */
+  //Create the associate transaction and sign with New Account's key 
+  const associateAccountTx = await new TokenAssociateTransaction()
+    .setAccountId(newAccountId)
+    .setTokenIds([tokenId])
+    .freezeWith(client)
+    .sign(newAccountPrivateKey);
+
+  //Submit the transaction to a Hedera network
+  const associateAccountTxSubmit = await associateAccountTx.execute(client);
+
+  //Get the transaction receipt
+  const associateAccountRx = await associateAccountTxSubmit.getReceipt(client);
+
+  //Confirm the transaction was successful
+  console.log(`NFT association with New Account's account: ${associateAccountRx.status}\n`);
+
+  /* ---------------- Transfer the NFT ---------------- */
+  // Check the balance before the transfer for the treasury account
+  var balanceCheckTx = await new AccountBalanceQuery().setAccountId(myAccountId).execute(client);
+  console.log(`Treasury balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
+
+  // Check the balance before the transfer for New Account's
+  var balanceCheckTx = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
+  console.log(`New Account's balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
+
+  // Transfer the NFT from treasury to New Account
+  // Sign with the treasury key to authorize the transfer
+  const tokenTransferTx = await new TransferTransaction()
+    .addNftTransfer(tokenId, 1, myAccountId, newAccountId)
+    .freezeWith(client)
+    .sign(PrivateKey.fromString(myPrivateKey));
+
+  const tokenTransferSubmit = await tokenTransferTx.execute(client);
+  const tokenTransferRx = await tokenTransferSubmit.getReceipt(client);
+
+  console.log(`\nNFT transfer from Treasury to New Account: ${tokenTransferRx.status} \n`);
+
+  // Check the balance of the treasury account after the transfer
+  var balanceCheckTx = await new AccountBalanceQuery().setAccountId(myAccountId).execute(client);
+  console.log(`Treasury balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
+
+  // Check the balance of New account's after the transfer
+  var balanceCheckTx = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
+  console.log(`New Accoun's balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
+
 
 }
-environmentSetup();
+nftCreate();
